edf097afe743aec8ddc9aa5a49f795876d9fbb83
Nemanja Lakicevic <n@lakicevic.no>
2021-12-29 23:28:03 +0100

    smui init

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,79 @@
 <script lang="ts">
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+
+  var screenWidth: number = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  let canvasWidth = 100 - (Math.log(screenWidth**5));
 </script>
 
 <main>
   <div class="header"></div>
   <div class="canvas-container">
-    <canvas id="canvas" >$0</canvas>
+    <canvas id="canvas" style="width: {canvasWidth}%">$0</canvas>
   </div>
+  <LayoutGrid>
+    {#each Array(2) as _unused, _i}
+      <Cell span={3} md=4>
+        <div class="button-cell">
+          LinkedIn
+        </div>
+      </Cell>
+    {/each}
+    <Cell spanDevices={{ desktop: 6, tablet: 2, phone: 2 }}>
+      <div class="button-cell">
+      Git
+      </div>
+    </Cell>
+    <Cell spanDevices={{ desktop: 3, tablet: 2, phone: 2 }}>
+      <div class="button-cell">
+      Soundcloud
+      </div>
+    </Cell>
+    <Cell spanDevices={{ desktop: 3, tablet: 2, phone: 2 }}>
+      <div class="button-cell">
+      StackOverflow
+      </div>
+    </Cell>
+  </LayoutGrid>
+
+  <LayoutGrid>
+  {#each Array(2) as _unused, _i}
+    <Cell span={6}>
+      <div class="demo-cell">Span 6</div>
+    </Cell>
+  {/each}
+  {#each Array(4) as _unused, _i}
+    <Cell span={3}>
+      <div class="demo-cell">Span 3</div>
+    </Cell>
+  {/each}
+  {#each Array(6) as _unused, _i}
+    <Cell span={2}>
+      <div class="demo-cell">Span 2</div>
+    </Cell>
+  {/each}
+  {#each Array(12) as _unused, _i}
+    <Cell span={1}>
+      <div class="demo-cell">Span 1</div>
+    </Cell>
+  {/each}
+  <Cell span={7}>
+    <div class="demo-cell">Span 7</div>
+  </Cell>
+  <Cell span={5}>
+    <div class="demo-cell">Span 5</div>
+  </Cell>
+  <Cell spanDevices={{ desktop: 6, tablet: 4, phone: 2 }}>
+    <div class="demo-cell" style="height: 80px;">
+      Span 6 on desktop, 4 on tablet, 2 on phone (always half)
+    </div>
+  </Cell>
+</LayoutGrid>
+
+
+
+  <!--
   <div class="info">
     <h>Nemanja Lakicevic</h>
     <div class=button-container>
@@ -40,7 +106,7 @@
       </li>
     </ul>
   </div>
-
+-->
 </main>
 
 <style>
@@ -55,12 +121,37 @@
     margin: 0 auto;
   }
 
+  .demo-cell {
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--mdc-theme-secondary, #333);
+    color: var(--mdc-theme-on-secondary, #fff);
+  }
+
   #canvas {
+    background: #ca0164;
     margin: auto;
-    width: 50%;
-    height: 280px;
     border: 2px solid darkgray;
     border-radius: 8px;
+    display: flex;
+    justify-content: center;
+  }
+
+  #canvas:before {
+    content:"";
+    display: block;
+    padding-top: 40%;
+  }
+
+   .button-cell {
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
+    color: white;
   }
 
   img {
--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,16 @@ import sveltePreprocess from 'svelte-preprocess'
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess(),
+  kit: {
+    // ... ignore the stuff here
+
+    // Add this part. ---
+    vite: {
+      ssr: {
+        noExternal: [/^@material\//, /^@smui(?:-extra)?\//],
+      },
+    },
+    // All done! ---
+  },
 }
